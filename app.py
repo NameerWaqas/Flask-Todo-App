c47d1fd5e460d45e5175a72adbc0883d20a9ed8a
NameerWaqas <nameerwaqas321@gmail.com>
2020-04-05 03:27:26 +0500

    All functionalities are working of todo app

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def index():
                       'Trusted_Connection=yes;')
     conn.autocommit = True
     cursor = conn.cursor()
+    # cursor.execute('DROP TABLE todoTable')
+    # cursor.execute('''CREATE TABLE todoTable(
+    #     todoID INT NOT NULL PRIMARY KEY IDENTITY,
+    #     todo TEXT
+    # ) ''')
     cursor.execute('SELECT * from todoTable')
 
     return render_template('index.html',todos=cursor.fetchall())
@@ -30,11 +35,38 @@ def handleHomePost():
         cursor.commit()        
     return redirect('/')
 
-@app.route('/update/')
+@app.route('/update/',methods=['GET','POST'])
 def handleUpdate():
+    if request.method=='POST':
+        todoID,message= request.form['updateID'].split(',')
+        conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=NameerPC\MSSQL;'
+                      'Database=tempDB;'
+                      'Trusted_Connection=yes;')
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute("update todoTable set todo = ? where todoID = ?",(message,int(todoID)))
+        cursor.commit()        
     return redirect('/')
 
-@app.route('/delete/')
+@app.route('/delete/',methods=['GET','POST'])
 def handleDelete():
-   return redirect('/')
+    if request.method == 'POST':
+        id = request.form['deleteID']
+        conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=NameerPC\MSSQL;'
+                      'Database=tempDB;'
+                      'Trusted_Connection=yes;')
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute("Delete From todoTable where todoID = ?",(int(id)))
+        cursor.commit()
+        cursor.execute("select COUNT(todoID) from todoTable")
+        maxID = cursor.fetchall()
+        cursor.execute("DBCC CHECKIDENT ('todoTable', RESEED, ?) ",(int(maxID[0][0])))
+        cursor.commit()        
+    return redirect('/')
+
+
+
 if __name__ =='__main__':    app.run(debug=True)
